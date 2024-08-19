20f106ad2fb153dd7fe77875767e2cd35b01cb8e
Iaromir <yaromirmorgan@gmail.com>
2024-08-19 22:21:45 +0300

    delete unuseful test

--- test/pass/test_jwt.rb
+++ test/pass/test_jwt.rb
@@ -6,8 +6,4 @@ class Pass::TestJwt < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Pass::Jwt::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
